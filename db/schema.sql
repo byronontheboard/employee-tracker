38115b3fa6ca68bdaeca403bda3d4cf7e1b73acc
byronallan <127366720+byronallan@users.noreply.github.com>
2023-08-29 14:09:35 -0700

    created tables for database

---
+++ db/schema.sql
@@ -0,0 +1,40 @@
+DROP DATABASE IF EXISTS employeeTracker_db
+CREATE DATABASE employeeTracker_db;
+
+CREATE TABLE departments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT
+);
+
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
