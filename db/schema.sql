2dd5dc3c4d40ef2d54743d1e91abc7e063c4caaa
byronallan <127366720+byronallan@users.noreply.github.com>
2023-08-29 14:16:15 -0700

    forgot to add FOREIGN KEY

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,7 @@
-DROP DATABASE IF EXISTS employeeTracker_db
+DROP DATABASE IF EXISTS employeeTracker_db;
 CREATE DATABASE employeeTracker_db;
+
+USE employeeTracker_db;
 
 CREATE TABLE departments (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -10,7 +12,8 @@ CREATE TABLE roles (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
-    department_id INT
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
@@ -19,6 +22,8 @@ CREATE TABLE employees (
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
 
 
